5c8a7a90311f2e88a3c35cd392cda3a2d460a008
Atif Anwer <atifanwer786@gmail.com>
2022-09-09 02:43:49 +0530

    Q2,Q3 fix

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -3,6 +3,8 @@ import scipy
 import matplotlib.pyplot as plt
 import pandas as pd
 import tqdm
+import sys
+plt.rcParams['agg.path.chunksize'] = 10000000000
 
 def prediction(parameters,x):
     return np.transpose(np.dot(parameters,np.transpose(x)))
@@ -13,93 +15,97 @@ def gradient_descent(learning_rate,parameters, y, x):
 def loss_function(parameters, y, x):
     return (1/(2*x.shape[0]))*np.sum(np.square(y-prediction(parameters,x)))
 
+def main():
+    test_data = sys.argv[1]
+    # Part a
+    parameters = np.array([3,1,2]).reshape((1,3))
 
-# Part a
-parameters = np.array([3,1,2]).reshape((1,3))
+    x = np.ones((1000000,3))
+    x[:,1] = np.random.normal(loc = 3, scale = 2, size = (1000000,))
+    x[:,2] = np.random.normal(loc = -1, scale = 2, size = (1000000,))
 
-x = np.ones((1000000,3))
-x[:,1] = np.random.normal(loc = 3, scale = 2, size = (1000000,))
-x[:,2] = np.random.normal(loc = -1, scale = 2, size = (1000000,))
+    y = prediction(parameters,x) 
 
-y = prediction(parameters,x) 
+    y = y + np.random.normal(loc = 0, scale = np.sqrt(2), size = (1000000,1))
 
-y = y + np.random.normal(loc = 0, scale = np.sqrt(2), size = (1000000,1))
-
-# part b
-all_parameters = []
-learning_rate = 0.01
+    # part b
+    all_parameters = []
+    learning_rate = 0.01
 
 
-# min_change = 0.001
-all_learning_history = []
+    # min_change = 0.001
+    all_learning_history = []
 
-def train(batch_size,learning_rate, max_iter):
+    def train(batch_size,learning_rate, max_iter):
+        
+        num_batches = int(x.shape[0]/batch_size)
+        x_batches = np.ones((num_batches,batch_size,x.shape[1]))
+        y_batches = np.ones((num_batches,batch_size,y.shape[1]))
+
+        for b in range(num_batches):
+            x_batches[b] = x[((b)*batch_size):((b+1)*batch_size)]
+            y_batches[b] = y[((b)*batch_size):((b+1)*batch_size)]
+        parameters = np.array([0,0,0]).reshape((1,3))
+        learning_history = []
+        min_change = 1e-4
+        batch_round_robin = 0
+        prev_epoch_loss = loss_function(parameters,y_batches[batch_round_robin],x[batch_round_robin])
+        current_epoch_loss = 0
+        for iter in tqdm.tqdm(range(max_iter)):
+            change = gradient_descent(learning_rate,parameters,y_batches[batch_round_robin],x_batches[batch_round_robin])
+            
+            learning_history.append(parameters)
+            
+            parameters = parameters+change
+            
+            batch_round_robin = (batch_round_robin + 1) % num_batches
+
+            current_epoch_loss += loss_function(parameters,y_batches[batch_round_robin],x[batch_round_robin])
+
+            if batch_round_robin == 0 :
+                current_epoch_loss /= num_batches
+                if(abs(current_epoch_loss-prev_epoch_loss)<min_change):
+                    break
+                prev_epoch_loss = current_epoch_loss
+                current_epoch_loss = 0
+
+        # print(parameters)
+        return parameters,learning_history
+
+    for batch_size in [1,100,10000,1000000]:
+        parameters,learning_history =  train(batch_size,learning_rate,10000000)
+        all_parameters.append(parameters)
+        all_learning_history.append(learning_history)
+        print(f"Batch Size : {batch_size} | Learned Parameters : {parameters}")
     
-    num_batches = int(x.shape[0]/batch_size)
-    x_batches = np.ones((num_batches,batch_size,x.shape[1]))
-    y_batches = np.ones((num_batches,batch_size,y.shape[1]))
+    # Test Dataset
 
-    for b in range(num_batches):
-        x_batches[b] = x[((b)*batch_size):((b+1)*batch_size)]
-        y_batches[b] = y[((b)*batch_size):((b+1)*batch_size)]
-    parameters = np.array([0,0,0]).reshape((1,3))
-    learning_history = []
-    min_change = 1e-5
-    print(x_batches[0])
-    batch_round_robin = 0
-    prev_epoch_loss = loss_function(parameters,y_batches[batch_round_robin],x[batch_round_robin])
-    current_epoch_loss = 0
-    for iter in tqdm.tqdm(range(max_iter)):
-        change = gradient_descent(learning_rate,parameters,y_batches[batch_round_robin],x_batches[batch_round_robin])
-        
-        learning_history.append(parameters)
-        
-        parameters = parameters+change
-        
-        batch_round_robin = (batch_round_robin + 1) % num_batches
+    linearX = pd.read_csv(f"{test_data}/X.csv",header = None)
+    data = linearX.to_numpy()
+    x_test = np.ones((data.shape[0],3))
+    x_test[:,1] = data[:,0]
+    x_test[:,2] = data[:,1]
+    
+    iter = 0
+    min_error = [1000,0]
+    for batch_size in [1,100,10000,1000000]:
+        error = loss_function(all_parameters[iter],y,x)
+        if(error < min_error[0]):
+            min_error = [error,iter]
+        iter = iter+1
+    np.savetxt("result_2.txt",prediction(all_parameters[min_error[1]],x),fmt = '%f')
 
-        current_epoch_loss += loss_function(parameters,y_batches[batch_round_robin],x[batch_round_robin])
+    # Learning Path
+    iter = 0
+    for batch_size in [1,100,10000,1000000]:
 
-        if batch_round_robin == 0 :
-            current_epoch_loss /= num_batches
-            if(abs(current_epoch_loss-prev_epoch_loss)<min_change):
-                break
-            prev_epoch_loss = current_epoch_loss
-            current_epoch_loss = 0
+        learning_history = np.squeeze(np.array(all_learning_history[iter]))
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    # print(parameters)
-    return parameters,learning_history
-
-for batch_size in [1,100,10000,1000000]:
-    parameters,learning_history =  train(batch_size,learning_rate,10000000)
-    all_parameters.append(parameters)
-    all_learning_history.append(learning_history)
-    print(f"Batch Size : {batch_size} | Learned Parameters : {parameters}")
-
-# Test Dataset
-
-linearX = pd.read_csv("q2test.csv")
-data = linearX.to_numpy()
-x = np.ones((data.shape[0],3))
-x[:,1] = data[:,0]
-x[:,2] = data[:,1]
-y = data[:,2].reshape((data.shape[0],1))
-
-print(f"Test Data Error using original hypothesis : {loss_function(np.array([3,1,2]).reshape((1,3)),y,x)}")
-
-iter = 0
-for batch_size in [1,100,10000,1000000]:
-    print(f"Batch Size : {batch_size} | Test Error : " + str(loss_function(all_parameters[iter],y,x)))
-    iter = iter+1
-
-
-# Learning Path
-iter = 0
-for batch_size in [1,100,10000,1000000]:
-
-    learning_history = np.squeeze(np.array(all_learning_history[iter]))
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-    ax.plot(learning_history[:,0],learning_history[:,1],learning_history[:,2])
-    plt.show()
-    iter = iter+1
+        ax.plot(learning_history[:,0],learning_history[:,1],learning_history[:,2])
+        fig.savefig(f"batch{batch_size}.png")
+        plt.close()
+        iter = iter+1
+if __name__ == '__main__':
+    main()
+    
--- Q3/Q3.py
+++ Q3/Q3.py
@@ -3,53 +3,88 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.linalg import pinv
+from scipy.linalg import pinv,norm
 import tqdm
+import sys
 
-def sigmoid(parameters,x):
-    return 1/(1+np.exp(-np.transpose(np.dot(parameters,np.transpose(x)))))
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
 
 def mean_normalization(x):
-    return (x-np.mean(x,axis=0))/np.std(x,axis=0),np.mean(x,axis=0),np.std(x,axis=0)
+    return (x-np.mean(x,axis=1).reshape(x.shape[0],1))/np.std(x,axis=1).reshape(x.shape[0],1),np.mean(x,axis=1).reshape(x.shape[0],1),np.std(x,axis=1).reshape(x.shape[0],1)
 
 def prediction(parameters,x):
-    return sigmoid(parameters,x)
+    return sigmoid((np.dot(parameters.T,x)))
 
 def gradient_descent(parameters, y, x):
-    hypothesis = prediction(parameters,x)
-    hessian = -np.dot((hypothesis*x).T,(1-hypothesis)*x)
-    Error = y - hypothesis
-    DelLL = np.sum(Error*x,axis = 0).T
-    return -np.dot(pinv(hessian),DelLL)
+    hypothesis = prediction(parameters,x).reshape((1,x.shape[1]))
+    hessian = -np.dot((hypothesis*x),((1-hypothesis)*x).T).reshape((parameters.shape[0],parameters.shape[0]))
+    Error = (y - hypothesis).reshape((1,x.shape[1]))
+    DelLL = np.sum(Error*x,axis = 1).reshape(parameters.shape)
+    return -np.dot(pinv(hessian),DelLL).reshape(parameters.shape)
 
-logisticX = pd.read_csv("logisticX.csv")
-logisticY = pd.read_csv("logisticY.csv")
+def main():
 
-x_data = logisticX.to_numpy()
-x,norm_mean, norm_std = mean_normalization(x_data)
-x_full = np.ones((x.shape[0],x.shape[1]+1))
-x_full[:,1:] = x
-x = x_full
-y = y_data = logisticY.to_numpy()
-# print(x,y)
+    train_data_directory = sys.argv[1]
+    test_data_directory = sys.argv[2]
+    logisticX = pd.read_csv(f"{train_data_directory}/X.csv",header=None)
+    logisticY = pd.read_csv(f"{train_data_directory}/Y.csv",header = None)
 
-parameters = np.zeros((1,x.shape[1]))
-for i in tqdm.tqdm(range(1000)):
-    change = gradient_descent(parameters,y,x)
-    parameters = parameters + change
-print(parameters)
+    x_data = logisticX.to_numpy()
+    x_data = x_data.T
+    x,norm_mean, norm_std = mean_normalization(x_data)
+    x_full = np.ones((x.shape[0]+1,x.shape[1]))
+    x_full[1:,:] = x[:]
+    x = x_full
 
-plt.rcParams['figure.dpi'] = 70
-plt.rcParams['figure.figsize']=[12,12]
-fig,ax = plt.subplots()
-ax.scatter(x_data[np.where(y==1),0],x_data[np.where(y==1),1],marker='x',label = "Category 1")
-ax.scatter(x_data[np.where(y==0),0],x_data[np.where(y==0),1],marker='s',label = "Category 0")
-x_axis = np.linspace(2,7,10)
-x_norm = (x_axis-norm_mean[0])/norm_std[0]
-y_norm = ((0.5-parameters[0,0])-parameters[0,1]*x_norm)/parameters[0,2]
-y_axis = y_norm*norm_std[1] + norm_mean[1]
-ax.plot(x_axis,y_axis,c = 'red', label = "Decision Boundary")
-ax.set_xlabel("x1")
-ax.set_ylabel("x2")
-ax.legend()
-plt.show()
+    y = y_data = logisticY.to_numpy()
+    y = y.T
+
+    
+    min_change = 1e-7
+
+    parameters = np.zeros((x.shape[0],1))
+    while True:
+        change = gradient_descent(parameters,y,x)
+        parameters = parameters + change.reshape(parameters.shape)
+        if norm(change)<min_change:
+            break
+    print(parameters)
+
+    plt.rcParams['figure.dpi'] = 70
+    plt.rcParams['figure.figsize']=[12,12]
+    fig,ax = plt.subplots()
+    ax.scatter(x_data[0,np.where(y==1)[1]],x_data[1,np.where(y==1)[1]],marker='x',label = "Category 1")
+    ax.scatter(x_data[0,np.where(y==0)[1]],x_data[1,np.where(y==0)[1]],marker='s',label = "Category 0")
+    x_axis = np.linspace(2,7,10)
+    x_norm = (x_axis-norm_mean[0])/norm_std[0]
+    y_norm = ((0.5-parameters[0,0])-parameters[1,0]*x_norm)/parameters[2,0]
+    y_axis = y_norm*norm_std[1] + norm_mean[1]
+    ax.plot(x_axis,y_axis,c = 'red', label = "Decision Boundary")
+    ax.set_xlabel("x1")
+    ax.set_ylabel("x2")
+    ax.set_title("Logistic Regression Decision Boundary")
+    ax.legend()
+
+    fig.savefig("Decision_Boundary.png")
+    plt.close()
+
+    # Test Data
+    logisticX = pd.read_csv(f"{test_data_directory}/X.csv",header=None)
+
+    x_data = logisticX.to_numpy()
+    x_data = x_data.T
+    x,norm_mean, norm_std = mean_normalization(x_data)
+    x_full = np.ones((x.shape[0]+1,x.shape[1]))
+    x_full[1:,:] = x[:]
+    x = x_full
+    y = prediction(parameters,x)
+
+    y[np.where(y>0.5)] = 1
+    y[np.where(y<=0.5)] = 0
+    np.savetxt("result_3.txt",y,fmt='%f')
+
+
+if __name__ == '__main__':
+    main()
+    
